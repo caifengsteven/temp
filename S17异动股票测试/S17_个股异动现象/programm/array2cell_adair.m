1b1b0771f8979bb95c6ebd8ed2ff8ab952dd075d
caifengsteven <31440870+caifengsteven@users.noreply.github.com>
2026-04-05 14:23:23 +0800

    xxx

--- S17异动股票测试/S17_个股异动现象/programm/array2cell_adair.m
+++
@@ -1,6 +0,0 @@
-function Y = array2cell_adair(y)
-n = size(y,2);
-Y = cell(1,n);
-for i = 1:n
-    Y{i} = y(:,i);
-end
--- S17异动股票测试/S17_个股异动现象/programm/deal_data1.m
+++
@@ -1,8 +0,0 @@
-function re1=deal_data1(re1)
-
-    re1 = [re1{:}]';
-
-    [~,ia] = sort(abs(cell2mat(re1(:,end))),'descend');
-
-    re1 = re1(ia,:);
-end
