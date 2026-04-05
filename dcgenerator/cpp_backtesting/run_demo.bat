43e715fab6f771c97137271319a0d299623724c1
caifengsteven <31440870+caifengsteven@users.noreply.github.com>
2026-04-05 10:15:37 +0800

    xxx

--- dcgenerator/cpp_backtesting/run_demo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Running DC Generator High-Frequency Trading Demo...
-echo.
-demo_dc_algorithm.exe
-echo.
-echo Demo completed. Press any key to exit.
-pause
--- dcgenerator/cpp_backtesting/test_us_explore.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Testing US Market Database Exploration
-echo.
-
-us_market_dc_test.exe --explore
-
-echo.
-echo Test completed.
-pause
