49669fdda48db0fd8fff94046e6e9ba2b3f1bc90
caifengsteven <31440870+caifengsteven@users.noreply.github.com>
2026-04-06 08:22:27 +0800

    x

--- S31/program/program/report_check_info.m
+++
@@ -1,7 +0,0 @@
-function report_check_info(OK,info)
-f_str = '核查：%s，结果：%s ';
-if OK
-    sprintf(f_str,info,'通过')
-else
-    sprintf(f_str,info,'不通过!!!!!,截图，或保留记录')
-end
--- s32/program/A_checktable.m
+++
@@ -1,7 +0,0 @@
-clear
-
-%ycz_min_history 数据库需要满
-%数据表
-dN_all = {'ycz_min_history'};
-tN_all = {'yuqerdata.yq_dayprice'};
-
